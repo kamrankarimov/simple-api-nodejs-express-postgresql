3ebee89c50bb9c1b90eddd17150bca36f1d66731
kamrankarimov <kamran.vecsiz@gmail.com>
2024-04-14 20:30:26 +0400

    dependencies: knex & pg package install

--- demo_data/products.js
+++ demo_data/products.js
@@ -4,21 +4,24 @@ var products = [
         title: "iPhone 11",
         description: "An apple mobile which is nothing like apple",
         price: 1100,
-        categoryId: 43
+        categoryId: 43,
+        brandId: 34
     },
     {
         id: 2,
         title: "LG Ultr HD",
         description: "New product very excellent",
         price: 560,
-        categoryId: 23
+        categoryId: 23,
+        brandId: 34
     },
     {
         id: 3,
         title: "Galaxy I342",
         description: "Smart Watch",
         price: 435,
-        categoryId: 54
+        categoryId: 54,
+        brandId: 34
     }
 ]
 
