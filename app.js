eb8544087455fdb1fdd5b5b409d6555f674f81b1
kamrankarimov <kamran.vecsiz@gmail.com>
2024-04-14 18:23:10 +0400

    get single product data json

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get('/products', (req, res) => {
     res.status(200).json(products)
 })
 
+app.get('/products/:id', (req, res) => {
+    const {id} = req.params
+    const product = products.find(product => product.id === parseInt(id))
+    if(product) res.status(200).json(product)
+    else res.status(404).json([{error: true, errMsg: `Product ID number ${id} was not found.`}])
+})
+
 app.listen(5000, () => {
     console.log('Server starting...')
 })
